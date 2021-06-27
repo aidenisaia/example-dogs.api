cec985afa40a2c8a539fd6457dc826063d64e812
Aiden Isaia <aidenisaia@gmail.com>
2021-06-27 09:25:56 -0700

    add dog and user models and routes

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,15 @@
+class DogsController < ApplicationController
+  def create
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed]
+      )
+      dog.save
+      render json: dog
+    else
+      render json: {message: "You must be logged in to create new dogs."}
+    end
+  end
+end
